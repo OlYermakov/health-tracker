53db7d9f9de813ac2b6ed311c6c4ecdc0a380f72
OlYermakov <130856904+OlYermakov@users.noreply.github.com>
2026-08-14 23:10:49 +0200

    chore: refresh sticky navigation cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME="health-tracker-v6";
+const CACHE_NAME="health-tracker-v7";
 const APP_SHELL=[
   "./","./index.html","./style.css?v=6","./training.css","./notes.css",
-  "./exercise-details.css","./enhancements.css?v=6","./app.js","./notes.js",
+  "./exercise-details.css","./enhancements.css?v=7","./app.js","./notes.js",
   "./exercise-details.js","./enhancements.js","./manifest.webmanifest","./icon.svg"
 ];
 
